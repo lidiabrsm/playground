087bab3848eaf850f87f3ad997580c8eabe62784
Lidia Brigido <lidiabrsm@gmail.com>
2019-04-15 23:05:32 -0300

    Updates

---
+++ precache-manifest.2af69a363d54b1529653eedfde335c15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/playground/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "49effa767ea8d7bd1610",
+    "url": "/playground/static/js/runtime~main.c80ac52c.js"
+  },
+  {
+    "revision": "8ec305957f2ea5a54a42",
+    "url": "/playground/static/js/main.0a2293dc.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/playground/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8ec305957f2ea5a54a42",
+    "url": "/playground/static/css/main.0bac3eab.chunk.css"
+  },
+  {
+    "revision": "90fa57f4f574c6db4919447fe24b964c",
+    "url": "/playground/index.html"
+  }
+];
